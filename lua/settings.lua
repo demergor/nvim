878beb95c7d1f954e9204470cd02c635ee072dab
JimNguy <jim.nguy39@gmail.com>
2025-12-14 23:55:54 +0100

    Added a column line to indicate an optimal max for line lengths

--- lua/settings.lua
+++ lua/settings.lua
@@ -4,6 +4,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
+vim.opt.colorcolumn = "100"
+
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
