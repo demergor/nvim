f64c45bdb037107495b884c3821671b31d74f73c
demergor <jim.nguy39@gmail.com>
2026-01-09 15:45:53 +0100

    Color tweaks mostly

--- init.lua
+++ init.lua
@@ -6,6 +6,14 @@ if vim.fn.has('macunix') == 1 then
   vim.env.PATH = "/Library/TeX/texbin:" .. vim.env.PATH
 end
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    vim.opt_local.number = true
+    vim.opt_local.relativenumber = true
+  end,
+})
+
 -- Modules
 require("settings")
 require("keymaps")
--- lua/colors.lua
+++ lua/colors.lua
@@ -2,14 +2,16 @@ local function my_colors()
   -- Text
   vim.api.nvim_set_hl(0, "Normal",            { fg = "#ffac69", bg = "#000000" })
   vim.api.nvim_set_hl(0, "Function",          { fg = "#b00b1e", bold = true })
-  vim.api.nvim_set_hl(0, "Number",            { fg = "#ffa7a9" })
-  vim.api.nvim_set_hl(0, "String",            { fg = "#9faf90" })
+  vim.api.nvim_set_hl(0, "String",            { fg = "#b1bea5" })
   vim.api.nvim_set_hl(0, "Comment",           { fg = "#898980", italic = true }) -- this is a comment
-  vim.api.nvim_set_hl(0, "Statement",         { fg = "#d1d5d2", bold = true })
-  vim.api.nvim_set_hl(0, "Special",           { fg = "#ff7739", bold = false })
+  vim.api.nvim_set_hl(0, "Statement",         { fg = "#e1e5e2", bold = true })
   vim.api.nvim_set_hl(0, "Type",              { fg = "#b69391", bold = true })
   vim.api.nvim_set_hl(0, "Delimiter",         { fg = "#efe9f4" })
   vim.api.nvim_set_hl(0, "Constant",          { fg = "#ffa7a9" })
+  vim.api.nvim_set_hl(0, "Number",            { fg = "#ffa7a9" })
+
+  vim.api.nvim_set_hl(0, "Special",           { fg = "#ff7739", bold = false })
+  vim.api.nvim_set_hl(0, "Type",              { fg = "#ff7739", bold = false })
 
   -- Cursor
   vim.api.nvim_set_hl(0, "Cursor",            { fg = "#000000", bg = "#ffffff" })
@@ -20,11 +22,14 @@ local function my_colors()
   -- Color column
   vim.api.nvim_set_hl(0, "ColorColumn",       { bg = "#110000" })
 
-  vim.api.nvim_set_hl(0, "@variable",         { fg = "#ede3e4" })
+  vim.api.nvim_set_hl(0, "@variable",         { fg = "#ffffff" })
   vim.api.nvim_set_hl(0, "Identifier",        { fg = "#ede3e4" })
 
   -- Status line
   vim.api.nvim_set_hl(0, "StatusLine",        { fg = "#ffffff", bg = "#110000", bold = true })
+
+  -- netrw
+  vim.api.nvim_set_hl(0, "Directory",         { fg = "#e1e5e2", bold = true })
 end
 
 my_colors()
