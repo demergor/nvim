50e572fb9a4ee5f72e3c1f9a1e3e9a0bc9fb2545
JimNguy <jim.nguy39@gmail.com>
2026-01-12 20:05:58 +0100

    Rehaul of colors to less saturated/muted colors to reduce halation and chromatic aberration Added rainbow braces (braces now alternate between whiteish - delimiter-color - and soft orange to help notice missing braces)

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,35 +1,42 @@
 local function my_colors()
-  -- Text
-  vim.api.nvim_set_hl(0, "Normal",            { fg = "#ffac69", bg = "#000000" })
-  vim.api.nvim_set_hl(0, "Function",          { fg = "#b00b1e", bold = true })
-  vim.api.nvim_set_hl(0, "String",            { fg = "#b1bea5" })
-  vim.api.nvim_set_hl(0, "Comment",           { fg = "#898980", italic = true }) -- this is a comment
-  vim.api.nvim_set_hl(0, "Statement",         { fg = "#e1e5e2", bold = true })
-  vim.api.nvim_set_hl(0, "Type",              { fg = "#b69391", bold = true })
-  vim.api.nvim_set_hl(0, "Delimiter",         { fg = "#efe9f4" })
-  vim.api.nvim_set_hl(0, "Constant",          { fg = "#ffa7a9" })
-  vim.api.nvim_set_hl(0, "Number",            { fg = "#ffa7a9" })
+  vim.api.nvim_set_hl(0, "Normal",        { fg = "#d8a06a", bg = "#000000" })
 
-  vim.api.nvim_set_hl(0, "Special",           { fg = "#ff7739", bold = false })
-  vim.api.nvim_set_hl(0, "Type",              { fg = "#ff7739", bold = false })
+  vim.api.nvim_set_hl(0, "Identifier",    { fg = "#ded6d0" })
+  vim.api.nvim_set_hl(0, "@variable",     { fg = "#e0e0e0" })
 
-  -- Cursor
-  vim.api.nvim_set_hl(0, "Cursor",            { fg = "#000000", bg = "#ffffff" })
-  vim.api.nvim_set_hl(0, "CursorLine",        { bg = "#181818" })
-  vim.api.nvim_set_hl(0, "CursorLineNr",      { fg = "#ffac69", bold = true })
-  vim.api.nvim_set_hl(0, "LineNr",            { fg = "#eeddbb" })
+  vim.api.nvim_set_hl(0, "Statement",     { fg = "#cfd8dc", bold = true })
+  vim.api.nvim_set_hl(0, "Function",      { fg = "#c15f5f", bold = true })
+  vim.api.nvim_set_hl(0, "Type",          { fg = "#e0703a", bold = false })
+  vim.api.nvim_set_hl(0, "Special",       { fg = "#e0703a", bold = false })
 
-  -- Color column
-  vim.api.nvim_set_hl(0, "ColorColumn",       { bg = "#110000" })
+  vim.api.nvim_set_hl(0, "String",        { fg = "#b1bea5" })
+  vim.api.nvim_set_hl(0, "Constant",      { fg = "#d89aa0" })
+  vim.api.nvim_set_hl(0, "Number",        { fg = "#d89aa0" })
 
-  vim.api.nvim_set_hl(0, "@variable",         { fg = "#ffffff" })
-  vim.api.nvim_set_hl(0, "Identifier",        { fg = "#ede3e4" })
+  vim.api.nvim_set_hl(0, "Comment",       { fg = "#8a8a82", italic = true })
 
-  -- Status line
-  vim.api.nvim_set_hl(0, "StatusLine",        { fg = "#ffffff", bg = "#110000", bold = true })
+  vim.api.nvim_set_hl(0, "Delimiter",     { fg = "#cfcac7" })
+
+  vim.api.nvim_set_hl(0, "Cursor",        { fg = "#000000", bg = "#d0d0d0" })
+
+  vim.api.nvim_set_hl(0, "CursorLine",    { bg = "#181818" })
+  vim.api.nvim_set_hl(0, "CursorLineNr",  { fg = "#d8a06a", bold = true })
+  vim.api.nvim_set_hl(0, "LineNr",        { fg = "#b2a18a" })
+
+  vim.api.nvim_set_hl(0, "ColorColumn",   { bg = "#110000" })
+  vim.api.nvim_set_hl(0, "StatusLine",    { fg = "#e0e0e0", bg = "#110000", bold = true })
+
+  -- Rainbow braces (alternating between white and orange to help detect missing braces)
+  vim.api.nvim_set_hl(0, "RainbowDelimiterRed",     { fg = "#cfcac7" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterYellow",  { fg = "#d6996b" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterBlue",    { fg = "#cfcac7" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterOrange",  { fg = "#d6996b" })
+  vim.api.nvim_set_hl(0, "RainbowDelimiterGreen",   { fg = "#cfcac7" })  
+  vim.api.nvim_set_hl(0, "RainbowDelimiterViolet",  { fg = "#d6996b" }) 
+  vim.api.nvim_set_hl(0, "RainbowDelimiterCyan",    { fg = "#cfcac7" })
 
   -- netrw
-  vim.api.nvim_set_hl(0, "Directory",         { fg = "#e1e5e2", bold = true })
+  vim.api.nvim_set_hl(0, "Directory",     { fg = "#dcdcdc", bold = true })
 end
 
 my_colors()
--- lua/old_colors.lua
+++ lua/old_colors.lua
@@ -1,83 +1,35 @@
 local function my_colors()
-  -- Base
-  vim.api.nvim_set_hl(0, "Normal",      { fg = "#faac69", bg = "#000000" }) -- warm beige main text
-  vim.api.nvim_set_hl(0, "CursorLine",  { bg = "#222222" })
-  vim.api.nvim_set_hl(0, "CursorLineNr",{ fg = "#ffdda0", bg = "#553322", bold = true }) -- bright warm line number
-  vim.api.nvim_set_hl(0, "LineNr",      { fg = "#eeddbb", bg = "#111111" }) -- subtle warm gray
+  -- Text
+  vim.api.nvim_set_hl(0, "Normal",            { fg = "#ffac69", bg = "#000000" })
+  vim.api.nvim_set_hl(0, "Function",          { fg = "#b00b1e", bold = true })
+  vim.api.nvim_set_hl(0, "String",            { fg = "#b1bea5" })
+  vim.api.nvim_set_hl(0, "Comment",           { fg = "#898980", italic = true }) -- this is a comment
+  vim.api.nvim_set_hl(0, "Statement",         { fg = "#e1e5e2", bold = true })
+  vim.api.nvim_set_hl(0, "Type",              { fg = "#b69391", bold = true })
+  vim.api.nvim_set_hl(0, "Delimiter",         { fg = "#efe9f4" })
+  vim.api.nvim_set_hl(0, "Constant",          { fg = "#ffa7a9" })
+  vim.api.nvim_set_hl(0, "Number",            { fg = "#ffa7a9" })
 
-  -- Selection
-  vim.api.nvim_set_hl(0, "Visual",      { bg = "#443322" })                  
-  vim.api.nvim_set_hl(0, "Search",      { fg = "#000000", bg = "#ffdd44", bold = true }) -- soft yellow highlight
-  vim.api.nvim_set_hl(0, "IncSearch",   { fg = "#000000", bg = "#ffee66", bold = true })
+  vim.api.nvim_set_hl(0, "Special",           { fg = "#ff7739", bold = false })
+  vim.api.nvim_set_hl(0, "Type",              { fg = "#ff7739", bold = false })
 
   -- Cursor
-  vim.api.nvim_set_hl(0, "Cursor",      { fg = "#000000", bg = "#ffffff" })
-  vim.api.nvim_set_hl(0, "MatchParen",  { bg = "#664466", bold = true })
+  vim.api.nvim_set_hl(0, "Cursor",            { fg = "#000000", bg = "#ffffff" })
+  vim.api.nvim_set_hl(0, "CursorLine",        { bg = "#181818" })
+  vim.api.nvim_set_hl(0, "CursorLineNr",      { fg = "#ffac69", bold = true })
+  vim.api.nvim_set_hl(0, "LineNr",            { fg = "#eeddbb" })
+
+  -- Color column
+  vim.api.nvim_set_hl(0, "ColorColumn",       { bg = "#390000" })
+
+  vim.api.nvim_set_hl(0, "@variable",         { fg = "#ffffff" })
+  vim.api.nvim_set_hl(0, "Identifier",        { fg = "#ede3e4" })
 
   -- Status line
-  vim.api.nvim_set_hl(0, "StatusLine",  { fg = "#e0e0e0", bg = "#111111", italic = true })
-  vim.api.nvim_set_hl(0, "StatusLineNC",{ fg = "#888888", bg = "#111111" })
-  vim.api.nvim_set_hl(0, "VertSplit",   { fg = "#444444", bg = "#000000" })
+  vim.api.nvim_set_hl(0, "StatusLine",        { fg = "#ffffff", bg = "#110000", bold = true })
 
-  -- Tabs
-  vim.api.nvim_set_hl(0, "TabLine",     { fg = "#ddccaa", bg = "#000000" })
-  vim.api.nvim_set_hl(0, "TabLineSel",  { fg = "#ffffff", bg = "#664466", bold = true })
-  vim.api.nvim_set_hl(0, "TabLineFill", { bg = "#000000" })
-
-  -- Popup menu
-  vim.api.nvim_set_hl(0, "Pmenu",       { fg = "#ddccaa", bg = "#111111" })
-  vim.api.nvim_set_hl(0, "PmenuSel",    { fg = "#ffffff", bg = "#664466", bold = true })
-  vim.api.nvim_set_hl(0, "PmenuThumb",  { bg = "#555555" })
-
-  -- Syntax highlighting (warm Gruvbox-inspired tones)
-  vim.api.nvim_set_hl(0, "@type", { fg = "#bf6969", bold = true })
-  vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#eebaa1", bold = true })
-  vim.api.nvim_set_hl(0, "@type.qualifier", { fg = "#efb0b0" })
-  vim.api.nvim_set_hl(0, "@type.identifier", { fg = "#b07e7e" })
-
-  vim.api.nvim_set_hl(0, "@keyword", { fg = "#eaeaea", bold = true })
-
-  vim.api.nvim_set_hl(0, "Comment",     { fg = "#a88866", italic = true }) -- muted warm brown
-  vim.api.nvim_set_hl(0, "Constant",    { fg = "#d27e99" }) -- pinkish
-  vim.api.nvim_set_hl(0, "String",      { fg = "#b0d070" }) -- warm green
-  vim.api.nvim_set_hl(0, "Character",   { fg = "#b2b873" })
-  vim.api.nvim_set_hl(0, "Number",      { fg = "#d27ec0" })
-  vim.api.nvim_set_hl(0, "Boolean",     { fg = "#ffaa66", bold = true })
-  vim.api.nvim_set_hl(0, "Function",    { fg = "#cd3939", bold = true }) -- desaturated teal
-  vim.api.nvim_set_hl(0, "Keyword",     { fg = "#dedede", bold = true }) 
-  vim.api.nvim_set_hl(0, "Type",        { fg = "#b07e7e", bold = true }) -- warm beige
-  vim.api.nvim_set_hl(0, "Identifier",  { fg = "#ffdda0" }) -- bright warm identifiers
-  vim.api.nvim_set_hl(0, "Operator",    { fg = "#ddccaa" })
-  vim.api.nvim_set_hl(0, "Title",       { fg = "#ffdd77", bold = true })
-  vim.api.nvim_set_hl(0, "Special",     { fg = "#ffdd77" })
-  vim.api.nvim_set_hl(0, "Underlined",  { fg = "#ffcc99", underline = true })
-  vim.api.nvim_set_hl(0, "Todo",        { fg = "#ffaa33", bold = true })
-  vim.api.nvim_set_hl(0, "Error",       { fg = "#ff6666", bold = true })
-  vim.api.nvim_set_hl(0, "WarningMsg",  { fg = "#ffbb33", bold = true })
-  vim.api.nvim_set_hl(0, "ErrorMsg",    { fg = "#ff6666", bg = "#111111", bold = true })
-
-  -- Fold and diff
-  vim.api.nvim_set_hl(0, "Folded",      { fg = "#888888", bg = "#111111" })
-  vim.api.nvim_set_hl(0, "DiffAdd",     { bg = "#223322" })
-  vim.api.nvim_set_hl(0, "DiffChange",  { bg = "#332222" })
-  vim.api.nvim_set_hl(0, "DiffDelete",  { bg = "#442222" })
-
-  -- Conceal / special chars
-  vim.api.nvim_set_hl(0, "Conceal",     { fg = "#777777" })
-  vim.api.nvim_set_hl(0, "SpecialKey",  { fg = "#555555" })
-
-  -- Quickfix / error list
-  vim.api.nvim_set_hl(0, "qfLineNr",    { fg = "#888888" })
-
-  -- Floating windows / virtual text
-  vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#ddccaa", bg = "#111111" })
-  vim.api.nvim_set_hl(0, "FloatBorder",{ fg = "#664466", bg = "#111111" })
-  vim.api.nvim_set_hl(0, "DiagnosticError",   { fg = "#ff6666" })
-  vim.api.nvim_set_hl(0, "DiagnosticWarn",    { fg = "#ffbb33" })
-  vim.api.nvim_set_hl(0, "DiagnosticInfo",    { fg = "#e6c07b" })
-  vim.api.nvim_set_hl(0, "DiagnosticHint",    { fg = "#ffdd77" })
-
-  vim.api.nvim_set_hl(0, "ColorColumn", {bg = "#110000" })
+  -- netrw
+  vim.api.nvim_set_hl(0, "Directory",         { fg = "#e1e5e2", bold = true })
 end
 
 my_colors()
